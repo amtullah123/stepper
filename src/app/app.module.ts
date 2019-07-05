fb47a10e16b973dac9ffecd05c1811ea04518a3c
amtullah123 <amtullah123@gmail.com>
2019-07-06 00:06:39 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,45 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StepperComponent } from './stepper/stepper.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatInputModule,MatButtonModule, MatIconModule} from '@angular/material';
+import {MatCardModule} from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatRadioModule} from '@angular/material/radio';
+import { MyDatePickerModule } from 'mydatepicker';
+import { MatRippleModule, MatDatepickerModule, MatNativeDateModule } from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StepperComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatStepperModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    FormsModule,
+     ReactiveFormsModule,
+     MatInputModule,
+     MatCardModule,
+     MatToolbarModule,
+     FlexLayoutModule,
+     MatRadioModule,
+     MyDatePickerModule,
+      MatDatepickerModule, 
+      MatNativeDateModule ,
+      MatRippleModule,
+      MatIconModule
+      
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
